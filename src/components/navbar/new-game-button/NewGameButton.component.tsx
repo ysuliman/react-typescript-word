407deafdd716cab34d02693849732ff11c645fa7
Yousef Suliman <yousef.f.suliman@gmail.com>
2022-04-10 02:25:40 -0700

    :bug: Closed #2.

--- src/components/navbar/new-game-button/NewGameButton.component.tsx
+++ src/components/navbar/new-game-button/NewGameButton.component.tsx
@@ -1,14 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import styles from './NewGameButton.module.css'
 import classNames from 'classnames'
 import { GameDispatchContext, GameStateContext } from '../../../context/GameStateProvider'
 
 const NewGameButton = () => {
     const { isLightMode } = useContext(GameStateContext)
-
     const gameDispatch = useContext(GameDispatchContext)
 
-    const handleClick = () => gameDispatch({ type: 'NEWGAME' })
+    const buttonRef = useRef<HTMLButtonElement>(null)
+
+    const handleClick = () => {
+        gameDispatch({ type: 'NEWGAME' })
+        setTimeout(() => {
+            buttonRef.current?.blur()
+        }, 500)
+    }
 
     return (
         <button
@@ -16,7 +22,9 @@ const NewGameButton = () => {
                 styles['new-game'],
                 isLightMode && styles.light
             )}
-            onClick={handleClick}>NEW GAME</button>
+            onClick={handleClick}
+            ref={buttonRef}
+        >NEW GAME</button>
     )
 }
 
