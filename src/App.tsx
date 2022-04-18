a675817e1719e2dc0c6a4f2983999ea53fff972b
Yousef Suliman <yousef.f.suliman@gmail.com>
2022-04-17 21:41:29 -0700

    :sparkles: Added ugly reactstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,76 @@ import { useCallback, useContext, useEffect } from 'react';
 import classNames from 'classnames';
 
 import AlertContainer from './components/alert/AlertContainer.component';
-import GuessGrid from './components/guess-grid/GuessGrid.component'
+import GuessGrid from './components/guess-grid/GuessGrid.component';
 import Keyboard from './components/keyboard/Keyboard.component';
-import Navbar from './components/navbar/Navbar.component';
-import { GameDispatchContext, GameStateContext } from './context/GameStateProvider';
+import NavbarOld from './components/navbar/Navbar.component';
+import {
+	GameDispatchContext,
+	GameStateContext,
+} from './context/GameStateProvider';
 
-import styles from './App.module.css'
+import styles from './App.module.css';
 import Footer from './components/footer/Footer.component';
 
 const App = () => {
-  const gameDispatch = useContext(GameDispatchContext)
-  const { isLightMode } = useContext(GameStateContext)
+	const gameDispatch = useContext(GameDispatchContext);
+	const { isLightMode } = useContext(GameStateContext);
 
-  // Workaround for notch on IOS
-  useEffect(() => {
-    document.body.style.transition = 'background-color .5s ease-in'
-  }, [])
-  useEffect(() => {
-    document.body.style.backgroundColor = isLightMode
-      ? '#fff'
-      : '#111112';
-  }, [isLightMode])
+	// Workaround for notch on IOS
+	useEffect(() => {
+		document.body.style.transition =
+			'background-color .5s ease-in';
+	}, []);
+	useEffect(() => {
+		document.body.style.backgroundColor = isLightMode
+			? '#fff'
+			: '#111112';
+	}, [isLightMode]);
 
-  const handleKeyPress = useCallback((ev: KeyboardEvent) => {
-    if (ev.key === 'Enter')
-      return gameDispatch({ type: 'SUBMIT' })
+	const handleKeyPress = useCallback(
+		(ev: KeyboardEvent) => {
+			if (ev.key === 'Enter')
+				return gameDispatch({ type: 'SUBMIT' });
 
-    if (ev.key === "Backspace" || ev.key === "Delete")
-      return gameDispatch({ type: 'DELETE' })
+			if (
+				ev.key === 'Backspace' ||
+				ev.key === 'Delete'
+			)
+				return gameDispatch({ type: 'DELETE' });
 
-    if (ev.key.match(/^[a-zA-Z]$/))
-      return gameDispatch({ type: 'LETTERPRESS', letter: ev.key })
-  }, [gameDispatch])
+			if (ev.key.match(/^[a-zA-Z]$/))
+				return gameDispatch({
+					type: 'LETTERPRESS',
+					letter: ev.key,
+				});
+		},
+		[gameDispatch]
+	);
 
-  // Set up keydown subscription
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress)
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress)
-    }
-  }, [handleKeyPress])
+	// Set up keydown subscription
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeyPress);
+		return () => {
+			window.removeEventListener(
+				'keydown',
+				handleKeyPress
+			);
+		};
+	}, [handleKeyPress]);
 
-
-  return (
-    <div className={classNames(styles.App, isLightMode && styles.light)}>
-      <Navbar />
-      <AlertContainer />
-      <GuessGrid />
-      <Keyboard />
-      <Footer />
-    </div>
-  );
-}
+	return (
+		<div
+			className={classNames(
+				styles.App,
+				isLightMode && styles.light
+			)}>
+			<NavbarOld />
+			<AlertContainer />
+			<GuessGrid />
+			<Keyboard />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
--- src/components/navbar/Navbar.component.tsx
+++ src/components/navbar/Navbar.component.tsx
@@ -1,25 +1,67 @@
-import React, { useContext } from 'react'
-import LightModeToggle from './light-mode-toggle/LightModeToggle.component'
-import styles from './Navbar.module.css'
-import classNames from 'classnames'
-import { GameStateContext } from '../../context/GameStateProvider'
-import NewGameButton from './new-game-button/NewGameButton.component'
-import Title from './title/Title.component'
-import FirebaseTest from '../../firebase/FirebaseTest'
+import React, { useContext, useState } from 'react';
+import LightModeToggle from './light-mode-toggle/LightModeToggle.component';
+import styles from './Navbar.module.css';
+import classNames from 'classnames';
+import { GameStateContext } from '../../context/GameStateProvider';
+import NewGameButton from './new-game-button/NewGameButton.component';
+import Title from './title/Title.component';
+import FirebaseTest from '../../firebase/FirebaseTest';
+import {
+	Button,
+	Collapse,
+	Container,
+	Nav,
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	NavItem,
+} from 'reactstrap';
+import SignInModal from '../../firebase/SignInModal/SignInModal';
 
-const Navbar = () => {
-    const { isLightMode } = useContext(GameStateContext)
+const NavbarOld = () => {
+	const { isLightMode } = useContext(GameStateContext);
 
-    return (
-        <nav className={classNames(styles.navbar, isLightMode && styles.light)}>
-            <div>
-                <NewGameButton />
-                <LightModeToggle />
-            </div>
-            <Title />
-            <FirebaseTest />
-        </nav>
-    )
-}
+	const [isToggleOpen, setIsToggleOpen] = useState(false);
 
-export default Navbar
+	const toggleNav = () =>
+		setIsToggleOpen(isOpen => !isOpen);
+
+	return (
+		<>
+			<Navbar
+				light={isLightMode}
+				dark={!isLightMode}
+				expand='md'
+				className={classNames(
+					styles.reactstrapnav
+				)}>
+				<NavbarBrand
+					className={classNames(
+						styles.title,
+						isLightMode && styles.light
+					)}>
+					Word
+				</NavbarBrand>
+
+				<NavbarToggler
+					onClick={toggleNav}></NavbarToggler>
+
+				<Collapse isOpen={isToggleOpen} navbar>
+					<Nav navbar>
+						<NavItem>
+							<FirebaseTest />
+						</NavItem>
+						<NavItem>
+							<NewGameButton />
+						</NavItem>
+						<NavItem>
+							<LightModeToggle />
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</>
+	);
+};
+
+export default NavbarOld;
